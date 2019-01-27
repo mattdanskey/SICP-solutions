fbae0d9775edea5335820b771a8e1cd200f652f8
Matthew Danskey <mdanskey@gmail.com>
2019-01-26 18:00:31 -0800

    finish 2.29.scm

--- chapter-2/2.2/2.29.scm
+++ chapter-2/2.2/2.29.scm
@@ -16,20 +16,55 @@
   (car (cdr branch)))
 
 ; 2)
-(define (total-weight mobile)
-  (cond ((not (pair? mobile)) mobile)
+(define (total-weight structure)
+  (cond ((not (pair? structure)) structure)
 	(else (+ (total-weight (branch-structure
-				 (left-branch mobile)))
+				 (left-branch structure)))
 		 (total-weight (branch-structure
-				 (right-branch mobile)))))))
+				 (right-branch structure)))))))
 
 ; 3)
-; (define (balanced? mobile)
-;  (
+; balanced? returns the combined weight of the branches
+; so that the torque of branches made from sub mobiles
+; make sense.
+; If not balanced in any structure, balanced? returns #f.
+(define (balanced? structure)
+  (cond ((not (pair? structure)) structure)
+	(else 
+	  (let ; apparently there's a let*, but not introduced (yet?) 
+	    ((left (left-branch structure))
+	     (right (right-branch structure)))
+	    (let
+	       ((left-length (branch-length left))
+		(right-length (branch-length right))
+		(left-weight (balanced? (branch-structure left)))
+		(right-weight (balanced? (branch-structure right))))
+	       (cond ((not (and left-weight right-weight))
+		       #f)
+		      ((not (= (* left-length left-weight)
+			       (* right-length right-weight)))
+		       #f)
+		      (else (+ left-weight right-weight))))))))
 
+(define s (make-mobile
+	    (make-branch 1 12)
+	    (make-branch 3 4)))
 (define a (make-mobile
 	    (make-branch 1 2)
 	    (make-branch 3 4)))
 (define b (make-mobile
 	    (make-branch 2 a)
 	    (make-branch 4 a)))
+
+(balanced? s)
+(balanced? a)
+(balanced? b)
+(balanced? (make-mobile
+	     (make-branch 2 s)
+	     (make-branch 3 s)))
+(balanced? (make-mobile
+	     (make-branch 2 s)
+	     (make-branch 2 s)))
+
+;4) Just right-branch and branch-structure would need to be updated
+; to remove the `car` calls
