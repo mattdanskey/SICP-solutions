8a7048f99f96fbd74beaef5269e559828ed11972
Matthew Danskey <mdanskey@gmail.com>
2019-01-29 18:23:55 -0800

    finish 2.32

--- chapter-2/2.2/2.32.scm
+++ chapter-2/2.2/2.32.scm
@@ -1,13 +1,14 @@
 #lang sicp
 
 (define (subsets s)
-  (define (f x) (display "magic here"))
   (if (null? s)
     (list nil)
     (let ((rest (subsets (cdr s))))
       (append rest
-	      (map f rest)))))
+	      (map (lambda (x) (cons (car s) x))
+		   rest)))))
 
+; more easily readable output
 (define (p s)
   (for-each (lambda (x)
 	      (display x)
@@ -15,3 +16,10 @@
 	    s))
 
 (p (subsets (list 1 2 3)))
+
+; It works by having `rest` be all the subsets without the first element,
+; and then adding to that all of those sets with the first element added.
+; It breaks down the problem by adding an additional combinatory item
+; each time we come back up the call stack one step (after first going
+; down calls to `subsets' to where nil is the only item and a list of
+; nil is the only set).
