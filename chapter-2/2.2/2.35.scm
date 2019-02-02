9ac2b1a579f3116147003e17387e5242290faa76
Matthew Danskey <mdanskey@gmail.com>
2019-02-01 23:39:48 -0800

    add exercise 2.35

---
+++ chapter-2/2.2/2.35.scm
@@ -0,0 +1,20 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op 
+                      initial 
+                      (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append 
+               (enumerate-tree (car tree))
+               (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1)
+		       (enumerate-tree t))))
