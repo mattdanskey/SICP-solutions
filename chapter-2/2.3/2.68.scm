3b3b2c3d65f7426646056c53bffad446c1958546
Matt Danskey <mdanskey@gmail.com>
2019-08-12 11:55:09 -0700

    (2.68) add exercise

---
+++ chapter-2/2.3/2.68.scm
@@ -0,0 +1,100 @@
+#lang sicp
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+	right
+	(append (symbols left) (symbols right))
+	(+ (weight left) (weight right))))
+
+(define (left-branch
+	  tree) (car
+	  tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+	'()
+	(let ((next-branch
+		(choose-branch (car bits) current-branch)))
+	  (if (leaf? next-branch)
+	      (cons (symbol-leaf next-branch)
+		    (decode-1 (cdr bits) tree))
+	      (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+	((= bit 1) (right-branch branch))
+	(else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set))) (cons x set))
+	(else (cons (car set)
+		    (adjoin-set x (cdr set))))))
+
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair)
+			       (cadr pair))
+		    ; symbol
+		    ; frequency
+		    (make-leaf-set (cdr pairs))))))
+
+; sample tree and message to be decoded:
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+		  (make-code-tree
+		    (make-leaf 'B 2)
+		    (make-code-tree
+		      (make-leaf 'D 1)
+		      (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(display (decode sample-message sample-tree))
+; ADABBCA
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((equal? x (car set)) true)
+	(else (element-of-set? x (cdr set)))))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append
+	(encode-symbol (car message)
+		       tree)
+	(encode (cdr message tree)))))
+
+(define (encode-symbol symbol tree)
+  (let ((left (left-branch tree))
+	(right (right-branch tree)))
+    (cond ((element-of-set? symbol (symbols left))
+	   (if (leaf? left)
+	       (list 0)
+	       (cons 0 (encode-symbol symbol left))))
+	  ((element-of-set? symbol (symbols right))
+	   (if (leaf? right)
+	       (list 1)
+	       (cons 1 (encode-symbol symbol right))))
+	  (else (error)))))
+
+(define unencoded-message '(A D A B B C A))
